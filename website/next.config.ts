bba9685eabfa5e1ee97da65b712159645f4bf6e2
mutalvita-cyber <236385573+mutalvita-cyber@users.noreply.github.com>
2026-05-08 10:54:40 +0530

    k

--- website/next.config.ts
+++ website/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  outputFileTracingRoot: process.cwd(/*turbopackIgnore: true*/),
   experimental: {
     esmExternals: true,
   },
